105f5d9b5e4846caa3f0ebbb28e228014c456678
Pamela McA’Nulty <pamela@mcanulty.org>
2019-03-22 10:33:49 -0400

    update requirements files

--- run_example_test.bash
+++ run_example_test.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH="`(pwd)`"
+rm -rf send_file.txt || true
 python examples/mptools_example_client.py AUTO &
 python examples/mptools_example.py
+rm -rf send_file.txt || true
+
